9553671e0f6a4f2829f2cb4ac9bc3ce23bc16b92
Jordi Gaspar <jordi@satberral.com>
2020-07-29 15:51:54 +0200

    First commit!

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Blog # ----- Import de db/model Project to the admin page
 
-# Register your models here.
+admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField()
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
